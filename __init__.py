9e9c506a3eb5f2970a97d5010fda6cc016d6acec
Frank Davies <fdavies93@gmail.com>
2021-07-07 21:02:56 +0800

    Added menu setup based on Chinese Support Redux codebase; refactored GUI code into new file; created generic classes to handle dialog setup

--- __init__.py
+++ __init__.py
@@ -4,12 +4,10 @@ from aqt import mw
 from anki.cards import Card
 from aqt.qt import *
 from aqt.utils import showInfo, qconnect
+from anki.hooks import addHook, wrap
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-def add_action_to_menu(action, target_function):
-    qconnect(action.triggered, target_function)
-    mw.form.menuTools.addAction(action)
+from .gui import gui_man
 
 def notion_upload():
     pass
@@ -17,47 +15,5 @@ def notion_upload():
 def notion_download():
     pass
 
-class Upload_Dialog(QDialog):
-    def __init__(self, parent=None):
-        self.parent = parent
-        QDialog.__init__(self, parent, Qt.Window)
-    
-
-
-class Upload_Ui(object):
-    def __init__(self):
-        self.dialog = Upload_Dialog()
-
-    def setup_dialog(self, dialog):
-        dialog.setObjectName("dialog")
-        dialog.resize(279, 75)
-        self.widget = QWidget(dialog)
-        self.widget.setGeometry(QRect(0, 10, 271, 53))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QVBoxLayout(self.widget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        QMetaObject.connectSlotsByName(dialog)
-        dialog.exec()
-
-    def show_ui(self):
-        self.setup_dialog(self.dialog)
-
-class Download_Box:
-    def __init__(self, parent=None):
-        self.parent = parent
-        QDialog.__init__(self, parent, Qt.Window)
-
-
-    def show_ui(self):
-        pass
-
-
-def create_upload_box():
-    up = Upload_Ui()
-    up.show_ui()
-
-upload_action = QAction("Upload to Notion", mw)
-# download_action = QAction("Download from Notion", mw)
-add_action_to_menu(upload_action, create_upload_box)
-# add_action_to_menu(download_action, notion_download)
+addHook('profileLoaded', gui_man.load_menu)
+addHook('unloadProfile', gui_man.unload_menus)
---
+++ gui.py
@@ -0,0 +1,96 @@
+from aqt import mw
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QAction, QActionGroup, QMenu
+from anki.lang import _
+from aqt import mw
+from aqt.qt import *
+
+class Gui_Manager():
+    def __init__(self):
+        self.dialogs = {"Upload": a2n_Ui(), "Download": a2n_Ui(), "Settings": a2n_Ui()}
+
+    def load_menu(self):
+        for k in self.dialogs:
+            add_menu_item("anki2notion",k,self.dialogs[k].show)
+
+    def unload_menus(self):
+        for menu in mw.custom_menus.values():
+            mw.form.menubar.removeAction(menu.menuAction())
+        mw.custom_menus.clear()
+
+class a2n_Dialog(QDialog):
+    def __init__(self, parent=None):
+        self.parent = parent
+        QDialog.__init__(self, parent, Qt.Window)
+
+# This class is designed to be overridden; it just sets up a basic object structure for orchestrating dialogs
+class a2n_Ui(object):
+    def __init__(self):
+        self.dialog = a2n_Dialog()
+    
+    def setup_dialog(self, dialog):
+        dialog.setObjectName("dialog")
+        dialog.resize(640, 480)
+        self.widget = QWidget(dialog)
+        self.widget.setGeometry(QRect(0, 10, 640, 480))
+        self.widget.setObjectName("widget")
+        QMetaObject.connectSlotsByName(dialog)
+
+    def show(self):
+        self.setup_dialog(self.dialog)
+        self.dialog.exec()
+
+def add_menu(path):
+    if not hasattr(mw, 'custom_menus'):
+        mw.custom_menus = {}
+
+    if len(path.split('::')) == 2:
+        parent_path, child_path = path.split('::')
+        has_child = True
+    else:
+        parent_path = path
+        has_child = False
+
+    if parent_path not in mw.custom_menus:
+        parent = QMenu('&' + parent_path, mw)
+        mw.custom_menus[parent_path] = parent
+        mw.form.menubar.insertMenu(mw.form.menuTools.menuAction(), parent)
+
+    if has_child and (path not in mw.custom_menus):
+        child = QMenu('&' + child_path, mw)
+        mw.custom_menus[path] = child
+        mw.custom_menus[parent_path].addMenu(child)
+
+
+def add_menu_item(path, text, func, keys=None, checkable=False, checked=False):
+    action = QAction(text, mw)
+
+    if keys:
+        action.setShortcut(QKeySequence(keys))
+
+    if checkable:
+        action.setCheckable(checkable)
+        action.toggled.connect(func)
+        if not hasattr(mw, 'action_groups'):
+            mw.action_groups = {}
+        if path not in mw.action_groups:
+            mw.action_groups[path] = QActionGroup(None)
+        mw.action_groups[path].addAction(action)
+        action.setChecked(checked)
+    else:
+        action.triggered.connect(func)
+
+    if path == 'File':
+        mw.form.menuCol.addAction(action)
+    elif path == 'Edit':
+        mw.form.menuEdit.addAction(action)
+    elif path == 'Tools':
+        mw.form.menuTools.addAction(action)
+    elif path == 'Help':
+        mw.form.menuHelp.addAction(action)
+    else:
+        add_menu(path)
+        mw.custom_menus[path].addAction(action)
+
+# ---------------------------
+gui_man = Gui_Manager()
