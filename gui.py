5cecdf881750a3467f66d10be4a65a83f8df583b
Frank Davies <fdavies93@gmail.com>
2021-07-09 10:48:09 +0800

    Added GUI specs from QT Design; wrote lambdas to load autogenerated forms

--- gui.py
+++ gui.py
@@ -5,70 +5,34 @@ from anki.lang import _
 from aqt import mw
 from aqt.qt import *
 from .model import model
+from .download import Download_Ui
+from .upload import Upload_Ui
+from .settings import Settings_Ui
 
 class Gui_Manager():
     def __init__(self):
-        self.dialogs = {"Upload": Upload_Ui(), "Download": Download_Ui(), "Settings": Settings_Ui()}
+        self.dialog_classes = {"Upload": Upload_Ui(), "Download": Download_Ui(), "Settings": Settings_Ui()}
+        self.dialogs = {}
+        for k in self.dialog_classes:
+            self.dialogs[k] = a2n_Dialog()
+            self.dialog_classes[k].setupUi(self.dialogs[k])
 
     def load_menu(self):
         for k in self.dialogs:
-            add_menu_item("anki2notion",k,self.dialogs[k].show)
+            add_menu_item("anki2notion",k,self.show_form_factory(self.dialogs[k]))
 
     def unload_menus(self):
         for menu in mw.custom_menus.values():
             mw.form.menubar.removeAction(menu.menuAction())
         mw.custom_menus.clear()
 
+    def show_form_factory(self, form):
+        return lambda _: form.exec()
+
 class a2n_Dialog(QDialog):
-    def __init__(self, parent=None):
+     def __init__(self, parent=None):
         self.parent = parent
         QDialog.__init__(self, parent, Qt.Window)
-
-# This class is designed to be overridden; it just sets up a basic object structure for orchestrating dialogs
-class a2n_Ui(object):
-    def __init__(self):
-        self.dialog = a2n_Dialog()
-    
-    def setup_dialog(self, dialog):
-        dialog.setObjectName("dialog")
-        dialog.resize(640, 480)
-        self.widget = QWidget(dialog)
-        self.widget.setGeometry(QRect(0, 10, 640, 480))
-        self.widget.setObjectName("widget")
-        QMetaObject.connectSlotsByName(dialog)
-
-    def show(self):
-        self.setup_dialog(self.dialog)
-        self.dialog.exec()
-
-class Upload_Ui(a2n_Ui):
-    def setup_dialog(self, dialog):
-        pass
-
-class Download_Ui(a2n_Ui):
-    def setup_dialog(self, dialog):
-        pass
-
-class Settings_Ui(a2n_Ui):
-    def setup_dialog(self, dialog):
-        dialog.setObjectName("dialog")
-        dialog.resize(640, 480)
-        dialog.widget = QWidget(dialog)
-        dialog.widget.setGeometry(QRect(0, 10, 640, 480))
-        dialog.widget.setObjectName("widget")
-        dialog.qvbox = QVBoxLayout(dialog.widget)
-        dialog.qvbox.setObjectName("qvbox")
-        dialog.api_key_text = QLineEdit(model.config["notion_key"],dialog.widget)
-        dialog.api_key_text.setObjectName("api_key_text")
-        dialog.savebutton = QPushButton("Save",dialog.widget)
-        dialog.savebutton.setObjectName("savebutton")
-        dialog.cancelbutton = QPushButton("Cancel",dialog.widget)
-        dialog.cancelbutton.setObjectName("cancelbutton")
-
-        for o in (dialog.api_key_text, dialog.savebutton, dialog.cancelbutton):
-            dialog.qvbox.addWidget(o)
-
-        QMetaObject.connectSlotsByName(dialog)
 
 def add_menu(path):
     if not hasattr(mw, 'custom_menus'):
